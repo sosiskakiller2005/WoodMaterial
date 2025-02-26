47db39438343c82d9d3e7334073d177bd5b222a3
Влад Козлов <vkozlov15012005@gmail.com>
2025-02-26 20:38:33 +0300

    Добавьте файлы проекта.

---
+++ WoodMaterial/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WoodMaterial.Model;
+
+namespace WoodMaterial
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static WoodMaterialDbEntities _context;
+        public static WoodMaterialDbEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new WoodMaterialDbEntities();
+            }
+            return _context;
+        }
+    }
+}
---
+++ WoodMaterial/AppData/AuthorisationHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.RightsManagement;
+using System.Text;
+using System.Threading.Tasks;
+using WoodMaterial.Model;
+
+namespace WoodMaterial.AppData
+{
+    internal class AuthorisationHelper
+    {
+        private static WoodMaterialDbEntities _context = App.GetContext();
+        public static User selectedUser;
+        /// <summary>
+        /// Авторизует пользователя
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        
+        public static bool Authorise(string login, string password)
+        {
+            List<User> users = _context.User.ToList();
+
+            if (login == string.Empty || password == string.Empty)
+            {
+                MessageBoxHelper.Error("Не все поля для ввода были заполнены.");
+                return false;
+            }
+            else 
+            {
+                foreach (User user in users)
+                {
+                    if (user.Login == login && user.Password == password)
+                    {
+                        selectedUser = user;
+                        return true;
+                    }
+                }
+                if (selectedUser != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    MessageBoxHelper.Error("Неправильно введен логин или пароль.");
+                    return false;
+                }
+            }
+        }
+    }
+}
