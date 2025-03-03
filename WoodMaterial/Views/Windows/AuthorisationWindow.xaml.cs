faba196b747fc4b8a869a324f406b72e9165c917
Влад Козлов <vkozlov15012005@gmail.com>
2025-03-03 18:48:22 +0300

    Добавлены новые окна и стили, обновлены существующие
    - В `App.xaml` добавлен новый словарь ресурсов `CheckBoxStyle.xaml`.
    - В `ButtonStyle.xaml` изменены размеры, цвет фона границы и добавлено свойство `TextElement.Foreground`.
    - В `HyperLinkStyle.xaml` исправлено свойство `FontFamily` на `FontSize`, изменен цвет текста и добавлено свойство `TextDecorations`.
    - В `PasswordBoxStyle.xaml` и `TextBoxStyle.xaml` изменены цвета фона и границы.
    - В `AuthorisationWindow.xaml` изменено название окна, добавлено свойство `WindowStartupLocation`, добавлены обработчики событий для `TextBox` и `PasswordBox`, изменен стиль кнопки "ВОЙТИ".
    - В `AuthorisationWindow.xaml.cs` добавлен `using` для `WoodMaterial.AppData`, добавлены методы для обработки событий `GotFocus` и `LostFocus`, добавлена логика для открытия окон `ClientsWindow` и `SignUpWindow`.
    - В `WoodMaterial.csproj` добавлены новые файлы `ClientsWindow.xaml`, `ClientsWindow.xaml.cs`, `SignUpWindow.xaml`, `SignUpWindow.xaml.cs` и `CheckBoxStyle.xaml`.
    - Добавлены новые файлы `CheckBoxStyle.xaml`, `ClientsWindow.xaml`, `ClientsWindow.xaml.cs`, `SignUpWindow.xaml`, `SignUpWindow.xaml.cs`.

--- WoodMaterial/Views/Windows/AuthorisationWindow.xaml.cs
+++ WoodMaterial/Views/Windows/AuthorisationWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WoodMaterial.AppData;
 
 namespace WoodMaterial.Views.Windows
 {
@@ -26,7 +27,12 @@ namespace WoodMaterial.Views.Windows
 
         private void EntryBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (AuthorisationHelper.Authorise(LoginTb.Text, PasswordTb.Password))
+            {
+                ClientsWindow clientsWindow = new ClientsWindow();
+                clientsWindow.Show();
+                Close();
+            }
         }
 
         private void ForgotPasswordHl_Click(object sender, RoutedEventArgs e)
@@ -36,7 +42,41 @@ namespace WoodMaterial.Views.Windows
 
         private void SignUpHl_Click(object sender, RoutedEventArgs e)
         {
+            SignUpWindow signUpWindow = new SignUpWindow();
+            signUpWindow.Show();
+            Close();
+        }
 
+        private void LoginTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTb.Text == "Введите логин")
+            {
+                LoginTb.Text = string.Empty;
+            }
+        }
+
+        private void LoginTb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTb.Text == string.Empty)
+            {
+                LoginTb.Text = "Введите логин";
+            }
+        }
+
+        private void PasswordTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTb.Password == "Введите пароль")
+            {
+                PasswordTb.Password = string.Empty;
+            }
+        }
+
+        private void PasswordTb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTb.Password == string.Empty)
+            {
+                PasswordTb.Password = "Введите пароль";
+            }
         }
     }
 }
---
+++ WoodMaterial/Views/Windows/SignUpWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WoodMaterial.AppData;
+using WoodMaterial.Model;
+
+namespace WoodMaterial.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для SignUpWindow.xaml
+    /// </summary>
+    public partial class SignUpWindow : Window
+    {
+        private static WoodMaterialDbEntities _context = App.GetContext();
+        public SignUpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthoriseHl_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorisationWindow authorisationWindow = new AuthorisationWindow();
+            authorisationWindow.Show();
+            Close();
+        }
+
+        private void LoginTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTb.Text == "Введите логин")
+            {
+                LoginTb.Text = string.Empty;
+            }
+        }
+
+        private void LoginTb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (LoginTb.Text == string.Empty)
+            {
+                LoginTb.Text = "Введите логин";
+            }
+        }
+
+        private void PasswordTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTb.Password == "Введите пароль")
+            {
+                PasswordTb.Password = string.Empty;
+            }
+        }
+
+        private void PasswordTb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTb.Password == string.Empty)
+            {
+                PasswordTb.Password = "Введите пароль";
+            }
+        }
+
+        private void RepeatPasswordTb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (RepeatPasswordTb.Password == "Повторите пароль")
+            {
+                RepeatPasswordTb.Password = string.Empty;
+            }
+        }
+
+        private void RepeatPasswordTb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (RepeatPasswordTb.Password == string.Empty)
+            {
+                RepeatPasswordTb.Password = "Повторите пароль";
+            }
+        }
+
+        private void EntryBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTb.Text != string.Empty && PasswordTb.Password != string.Empty && RepeatPasswordTb.Password != string.Empty)
+            {
+                User newUser = new User()
+                {
+                    Login = LoginTb.Text,
+                    Password = PasswordTb.Password
+                };
+                _context.User.Add(newUser);
+                _context.SaveChanges();
+                MessageBoxHelper.Information("Регистрация прошла успешно");
+                AuthorisationWindow authorisationWindow = new AuthorisationWindow();
+            }
+        }
+
+        private void ForgotPasswordHl_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
